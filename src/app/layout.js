921d6cf0b112e4c3b0f67435982524cf26abbf5e
EntityJoKeR <133527371+EntityJoKeR@users.noreply.github.com>
2024-10-26 18:18:39 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+"use client"
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -20,6 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
