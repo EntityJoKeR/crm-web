316851401ff9c7f91ab20ce02d9d1781e84e930b
EntityJoKeR <133527371+EntityJoKeR@users.noreply.github.com>
2024-10-26 18:16:49 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
+  const tg = window.Telegram?.WebApp
+  console.log(tg)
   return (
-    <div className="flex w-full h-full items-center justify-center">
-      <p>e,fleoe</p>
+    <div className="w-full h-screen flex items-center justify-center">
+      <p>{tg.initData?`${tg.initDataUnsafe}`:"not tg"}</p>
     </div>
   );
 }
